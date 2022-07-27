c25dd70785e31646fa90ce49f52d0ace05c210e7
Boris Bo <bolsh.bo@yandex.ru>
2022-07-27 22:53:44 +0300

    rx intro

--- Rx/IntroRx/IntroRx/ViewController.swift
+++ Rx/IntroRx/IntroRx/ViewController.swift
@@ -25,7 +25,39 @@ struct ProductViewModel {
 }
 
 class ViewController: UIViewController {
+	private let tableView: UITableView = {
+		let table = UITableView()
+		table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+		return table
+	}()
+
+	private var viewModel = ProductViewModel()
+	private var bag = DisposeBag()
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		view.addSubview(tableView)
+		tableView.frame = view.bounds
+		bindTableData()
+	}
+
+	func bindTableData() {
+		// Bind items to table
+		viewModel.items.bind(
+			to: tableView.rx.items(
+				cellIdentifier: "cell",
+				cellType: UITableViewCell.self)
+		) { row, model, cell in
+			cell.textLabel?.text = model.title
+			cell.imageView?.image = UIImage(systemName: model.imageName)
+		}.disposed(by: bag)
+
+		// Bind a model selected handler
+		tableView.rx.modelSelected(Product.self).bind { product in
+			print(product.title)
+		}.disposed(by: bag)
+
+		// fetch items
+		viewModel.fetchItems()
 	}
 }
