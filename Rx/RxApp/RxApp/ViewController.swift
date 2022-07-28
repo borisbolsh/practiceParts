2f80925d6e5d9d8053b90e1c4c3d4739042ba619
Boris Bo <bolsh.bo@yandex.ru>
2022-07-28 08:36:16 +0300

    add restaurants.json

--- Rx/RxApp/RxApp/ViewController.swift
+++ Rx/RxApp/RxApp/ViewController.swift
@@ -1,10 +1,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+	static func instantiate() -> ViewController {
+		let storyboard = UIStoryboard(name: "Main", bundle: .main)
+		let viewController = storyboard.instantiateInitialViewController() as! ViewController
+		return viewController
+	}
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		view.backgroundColor = .blue
-		self.title = "main"
 	}
 }
