a382b2216d9b8dc96b65b271c8101ec1b18106fd
Boris Bo <bolsh.bo@yandex.ru>
2022-08-02 23:50:52 +0300

    add constants, btns targets, etc

--- Timers/Pomodoro/Pomodoro/ViewController.swift
+++ Timers/Pomodoro/Pomodoro/ViewController.swift
@@ -3,7 +3,20 @@ import UIKit
 final class ViewController: UIViewController {
 	private enum Constants {
 		static let topInsetTimeLabel: CGFloat = 20
+
+		enum CancelBtn {
+			static let height: CGFloat = 56
+			static let leftInset: CGFloat = 16
+			static let bottomInset: CGFloat = -16
+			static let rightInset: CGFloat = -16
+		}
+
+		enum StartBtn {
+			static let bottomInset: CGFloat = -16
+			static let rightInset: CGFloat = -16
+		}
 	}
+
 	private let timeLabel = UILabel()
 	private let startButton = UIButton()
 	private let cancelButton = UIButton()
@@ -19,7 +32,15 @@ final class ViewController: UIViewController {
 	}
 
 	private func setupSubviews() {
+		timeLabel.text = "25:00"
+		
+		startButton.backgroundColor = .black
+		startButton.setTitle("Start", for: .normal)
+		startButton.addTarget(self, action: #selector(startButtonTapped), for: .touchUpInside)
 
+		cancelButton.backgroundColor = .black
+		cancelButton.setTitle("Cancel", for: .normal)
+		cancelButton.addTarget(self, action: #selector(cancelButtonTapped), for: .touchUpInside)
 	}
 
 	private func setupUI() {
@@ -35,18 +56,29 @@ final class ViewController: UIViewController {
 			timeLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
 			timeLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: Constants.topInsetTimeLabel),
 
-			cancelButton.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 16),
-			cancelButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 16),
-			cancelButton.rightAnchor.constraint(equalTo: startButton.leftAnchor, constant: -16),
-			cancelButton.heightAnchor.constraint(equalToConstant: 56)
+			cancelButton.leftAnchor.constraint(equalTo: view.leftAnchor, constant: Constants.CancelBtn.leftInset),
+			cancelButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: Constants.CancelBtn.bottomInset),
+			cancelButton.rightAnchor.constraint(equalTo: startButton.leftAnchor, constant: Constants.CancelBtn.rightInset),
+			cancelButton.heightAnchor.constraint(equalToConstant: Constants.CancelBtn.height),
 
-			startButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -16),
-			startButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 16),
-			startButton.heightAnchor.constraint(equalTo: cancelButton.heightAnchor)
+			startButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: Constants.StartBtn.rightInset),
+			startButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: Constants.StartBtn.bottomInset),
+			startButton.heightAnchor.constraint(equalTo: cancelButton.heightAnchor),
+			startButton.widthAnchor.constraint(equalTo: cancelButton.widthAnchor)
 		])
 	}
 }
 
 // MARK: Actions
 
-extension ViewController {}
+extension ViewController {
+	@objc
+	private func startButtonTapped() {
+		print("startButtonTapped")
+	}
+
+	@objc
+	private func cancelButtonTapped() {
+		print("cancelButtonTapped")
+	}
+}
