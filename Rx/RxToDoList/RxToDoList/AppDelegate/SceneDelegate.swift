4d7a64f0ad8a9628006684fe01e922f4b6c8c025
Boris Bo <bolsh.bo@yandex.ru>
2022-09-04 23:12:39 +0300

    rx todo list

--- Rx/RxToDoList/RxToDoList/AppDelegate/SceneDelegate.swift
+++ Rx/RxToDoList/RxToDoList/AppDelegate/SceneDelegate.swift
@@ -6,7 +6,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 	func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
 		guard let scene = (scene as? UIWindowScene) else { return }
 
-		let vc = TasksListViewController()
+//		let vc = TasksListViewController()
+		let vc = LoginViewController()
 		let navVC = UINavigationController(rootViewController: vc)
 		
 		window = .init(windowScene: scene)
